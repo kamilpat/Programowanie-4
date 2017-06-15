90075bff7c7c1f0f1e6a30d30f8faed6b16de7df
Kamil <kubicakamil1994@gmail.com>
2017-06-15 23:58:41 +0200

    path

--- Porownywarka/Controler/DatabaseConnection.cs
+++ Porownywarka/Controler/DatabaseConnection.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Porownywarka.Controler
 {
   public  static class DatabaseConnection
   {
-      public static string Connection= @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=E:\Programownie\SearchedEngineDataBase.mdf;Integrated Security = True; Connect Timeout = 30";
+      private static readonly string  path=Path.GetFullPath(@"..\..\..\..\..\SearchedEngineDataBase.mdf");
+      private static string Connection= @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename="+path+";Integrated Security = True; Connect Timeout = 30";
       public static LINQToSQLClassDataContext dc = new LINQToSQLClassDataContext(Connection);
     }
 }
